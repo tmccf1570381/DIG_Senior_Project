c7659e7558c8ed8836976e1065ce4525e2ba6bb7
tmccf1570381 <makoto_tanabe@mail.toyota.co.jp>
2023-06-20 20:34:24 +0900

    aws test2

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 cd /var/www/html/
 # pm2 start npm --name dig-so-2023-06 
 # pm2 start npm --name dig-sp-2023-06 -- run server
-pm2 start npm --name dig-senior -- run start
+# pm2 start npm --name dig-senior -- run start
