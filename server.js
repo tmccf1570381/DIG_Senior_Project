4bd85b54fb62b3d483edc8417fe32283dc57c69d
tmccf1570381 <makoto_tanabe@mail.toyota.co.jp>
2023-06-21 14:09:44 +0900

    next

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,"./build")));
+// app.use(express.static(path.join(__dirname,"./build")));
 
 app.get('/', (req, res) => {
   res.send('Hello');
